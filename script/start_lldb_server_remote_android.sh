8562266d01906fd9b84d8bd820b5b5e03ae5019f
pengfei <pengfei.02@bytedance.com>
2019-04-01 21:50:16 +0800

    android debug

---
+++ script/start_lldb_server_remote_android.sh
@@ -0,0 +1,14 @@
+
+echo "mkdir"
+adb shell "mkdir -p /data/local/tmp/lldb/bin"
+adb shell "mkdir -p /data/local/tmp/lldb/tmp"
+adb shell "mkdir -p /data/local/tmp/lldb/log"
+echo "push lldb-server"
+adb push /Users/pengfei/Library/Android/sdk/lldb/3.1/android/arm64-v8a/lldb-server /data/local/tmp/lldb/bin/
+adb push /Users/pengfei/Library/Android/sdk/lldb/3.1/android/start_lldb_server.sh /data/local/tmp/lldb/bin/
+adb shell "chmod a+x /data/local/tmp/lldb/bin/lldb-server"
+adb shell "chmod a+x /data/local/tmp/lldb/bin/start_lldb_server.sh"
+echo "kill ps"
+adb shell "pkill lldb-server"
+echo "start lldb-server"
+adb shell "sh /data/local/tmp/lldb/bin/start_lldb_server.sh /data/local/tmp/lldb unix-abstract /data/local/tmp/lldb/tmp debug.sock"
