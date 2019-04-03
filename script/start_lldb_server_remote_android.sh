f8cda15a0f6c7119bf91ea0b41dd8b743ec6ced2
pengfei <pengfei.02@bytedance.com>
2019-04-03 17:54:41 +0800

    support debug android

--- script/start_lldb_server_remote_android.sh
+++ script/start_lldb_server_remote_android.sh
@@ -1,14 +1,11 @@
 
-echo "mkdir"
-adb shell "mkdir -p /data/local/tmp/lldb/bin"
-adb shell "mkdir -p /data/local/tmp/lldb/tmp"
-adb shell "mkdir -p /data/local/tmp/lldb/log"
-echo "push lldb-server"
-adb push /Users/pengfei/Library/Android/sdk/lldb/3.1/android/arm64-v8a/lldb-server /data/local/tmp/lldb/bin/
-adb push /Users/pengfei/Library/Android/sdk/lldb/3.1/android/start_lldb_server.sh /data/local/tmp/lldb/bin/
-adb shell "chmod a+x /data/local/tmp/lldb/bin/lldb-server"
-adb shell "chmod a+x /data/local/tmp/lldb/bin/start_lldb_server.sh"
-echo "kill ps"
-adb shell "pkill lldb-server"
-echo "start lldb-server"
-adb shell "sh /data/local/tmp/lldb/bin/start_lldb_server.sh /data/local/tmp/lldb unix-abstract /data/local/tmp/lldb/tmp debug.sock"
+#!/bin/sh
+
+sdk="/Users/pengfei/Library/Android/sdk"
+pkg="com.example.pengfei.ndkdebug"
+
+adb shell 'pkill lldb-server'
+adb shell "run-as ${pkg} sh -c 'pkill lldb-server'"
+adb push "${sdk}/lldb/3.1/android/armeabi/lldb-server" /data/local/tmp/lldb-server
+adb shell "cat /data/local/tmp/lldb-server | run-as ${pkg} sh -c 'cat > /data/data/${pkg}/lldb/bin/lldb-server && chmod 700 /data/data/${pkg}/lldb/bin/lldb-server'"
+adb shell "run-as ${pkg} sh -c '/data/data/${pkg}/lldb/bin/lldb-server platform --server --listen *:9527'"
