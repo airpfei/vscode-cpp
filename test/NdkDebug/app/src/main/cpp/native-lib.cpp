e234ee3f1576ab2899ea4bbb6c471e436016469b
pengfei <pengfei.02@bytedance.com>
2019-04-04 13:09:43 +0800

    add debug demo

---
+++ test/NdkDebug/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,97 @@
+#include <jni.h>
+#include <string>
+
+#include <android/log.h>
+
+#include <stdlib.h>
+
+#define LOG_TAG "simplejni native.cpp"
+
+#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_pengfei_ndkdebug_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+
+//    int *ptr = (int*)malloc(sizeof(int) * 3);
+//    ptr[4] = 6;
+
+
+    return env->NewStringUTF(hello.c_str());
+}
+
+static jint addpengfei(JNIEnv* /*env*/, jobject /*thiz*/, jint a, jint b) {
+    int result = a + b + 10;
+    ALOGI("%d + %d = %d", a, b, result);
+
+    int *ptr = (int*)malloc(sizeof(int) * 100);
+    ptr[4] = 6;
+
+    return result;
+}
+static const char *classPathName = "com/example/pengfei/ndkdebug/MainActivity";
+static JNINativeMethod methods[] = {
+        {"addpengfei", "(II)I", (void*)addpengfei },
+};
+
+static int registerNativeMethods(JNIEnv* env, const char* className,
+                                 JNINativeMethod* gMethods, int numMethods) {
+    jclass clazz;
+    clazz = env->FindClass(className);
+    if (clazz == NULL) {
+        ALOGE("Native registration unable to find class '%s'", className);
+        return JNI_FALSE;
+    }
+
+    if (env->RegisterNatives(clazz, gMethods, numMethods) < 0) {
+        ALOGE("RegisterNatives failed for '%s'", className);
+        return JNI_FALSE;
+    }
+    return JNI_TRUE;
+}
+/*
+ * Register native methods for all classes we know about.
+ *
+ * returns JNI_TRUE on success.
+ */
+static int registerNatives(JNIEnv* env)  {
+    if (!registerNativeMethods(env, classPathName,
+                               methods, sizeof(methods) / sizeof(methods[0]))) {
+        return JNI_FALSE;
+    }
+    return JNI_TRUE;
+}
+
+typedef union {
+    JNIEnv* env;
+    void* venv;
+} UnionJNIEnvToVoid;
+
+jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    UnionJNIEnvToVoid uenv;
+    uenv.venv = NULL;
+    jint result = -1;
+    JNIEnv* env = NULL;
+
+    ALOGI("JNI_OnLoad");
+    if (vm->GetEnv(&uenv.venv, JNI_VERSION_1_4) != JNI_OK) {
+        ALOGE("ERROR: GetEnv failed");
+        goto bail;
+    }
+    env = uenv.env;
+    if (registerNatives(env) != JNI_TRUE) {
+        ALOGE("ERROR: registerNatives failed");
+        goto bail;
+    }
+
+    result = JNI_VERSION_1_4;
+
+    bail:
+    return result;
+}
