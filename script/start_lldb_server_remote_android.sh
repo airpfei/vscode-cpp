4ae9016ff20a8bcd25b08128bc774bcb7379e276
pengfei <pengfei.02@bytedance.com>
2019-04-04 14:28:18 +0800

    use unix-abstract-connect protocol

--- script/start_lldb_server_remote_android.sh
+++ script/start_lldb_server_remote_android.sh
@@ -3,11 +3,9 @@
 
 sdk="/Users/pengfei/Library/Android/sdk"
 pkg="com.example.pengfei.ndkdebug"
-port=9527
 
 adb shell 'pkill lldb-server'
 adb shell "run-as ${pkg} sh -c 'pkill lldb-server'"
-adb forward tcp:${port} tcp:${port}
 adb push "${sdk}/lldb/3.1/android/armeabi/lldb-server" /data/local/tmp/lldb-server
 adb shell "cat /data/local/tmp/lldb-server | run-as ${pkg} sh -c 'mkdir -p lldb/bin/ && cat > /data/data/${pkg}/lldb/bin/lldb-server && chmod 700 /data/data/${pkg}/lldb/bin/lldb-server'"
-adb shell "run-as ${pkg} sh -c '/data/data/${pkg}/lldb/bin/lldb-server platform --server --listen *:${port}'"
+adb shell "run-as ${pkg} sh -c 'lldb/bin/lldb-server platform --server --listen unix-abstract:///data/local/tmp/debug.sock'"
