f7b742228fe469dc8829a6f4f2062cb62c7c0330
pengfei <pengfei.02@bytedance.com>
2019-04-04 13:58:10 +0800

    update script

--- script/start_lldb_server_remote_android.sh
+++ script/start_lldb_server_remote_android.sh
@@ -9,5 +9,5 @@ adb shell 'pkill lldb-server'
 adb shell "run-as ${pkg} sh -c 'pkill lldb-server'"
 adb forward tcp:${port} tcp:${port}
 adb push "${sdk}/lldb/3.1/android/armeabi/lldb-server" /data/local/tmp/lldb-server
-adb shell "cat /data/local/tmp/lldb-server | run-as ${pkg} sh -c 'cat > /data/data/${pkg}/lldb/bin/lldb-server && chmod 700 /data/data/${pkg}/lldb/bin/lldb-server'"
+adb shell "cat /data/local/tmp/lldb-server | run-as ${pkg} sh -c 'mkdir -p lldb/bin/ && cat > /data/data/${pkg}/lldb/bin/lldb-server && chmod 700 /data/data/${pkg}/lldb/bin/lldb-server'"
 adb shell "run-as ${pkg} sh -c '/data/data/${pkg}/lldb/bin/lldb-server platform --server --listen *:${port}'"
